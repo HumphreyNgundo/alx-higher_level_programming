c7ec90d8f3291e13f55637034291ea4d9f2d1f7f
HumphreyNgundo <humphreyngundo28@gmail.com>
2023-01-09 20:30:47 +0000

    task files

---
+++ 0x0A-python-inheritance/tests/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return dir(obj)#!/usr/bin/python3
+
