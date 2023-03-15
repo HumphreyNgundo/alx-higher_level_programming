aa3ddf97bed8f40e296b895981305163dcca38bb
HumphreyNgundo <humphreyngundo28@gmail.com>
2023-03-15 13:17:37 -0700

    adding 1-multi_languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const aboutC = "C is fun";
+const aboutPy = "Python is cool";
+const aboutJs = "JavaScript is amazing";
+console.log(aboutC);
+console.log(aboutPy);
+console.log(aboutJs);
