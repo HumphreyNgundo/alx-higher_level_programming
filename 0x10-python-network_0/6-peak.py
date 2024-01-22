f82061d3f740928339904037b632d92b058dace2
Humphrey <humphreyngundo28@gmail.com>
2024-01-22 22:37:50 +0300

    max function used

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """ Finds a peak inside a list """
-
-
 def find_peak(list_of_integers):
-    if list_of_integers == []:
+    if not list_of_integers:
         return None
-    max = list_of_integers[0]
-    for item in list_of_integers:
-        if item > max:
-            max = item
-    return max
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
